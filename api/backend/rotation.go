c73e1dd9a19caaf65c4ee854f5f615b5a396893c
weibin <xiaobinqt@163.com>
2024-06-30 18:31:11 +0800

    :fire: study day04

--- api/backend/rotation.go
+++ api/backend/rotation.go
@@ -23,3 +23,12 @@ type RotationDeleteReq struct {
 }
 
 type RotationDeleteRes struct{}
+
+type RotationUpdateReq struct {
+	g.Meta `path:"/backend/rotation/update/{Id}" method:"post" tags:"轮播图" summary:"修改轮播图接口"`
+	Id     int    `json:"id"      v:"min:1#请选择需要修改的内容" dc:"轮播图Id"`
+	PicUrl string `json:"pic_url"    v:"required#图片链接不能为空" dc:"图片链接"`
+	Link   string `json:"link"    v:"required#图片跳转链接不能为空" dc:"跳转链接"`
+	Sort   int    `json:"int"   dc:"排序"`
+}
+type RotationUpdateRes struct{}
--- internal/model/rotation.go
+++ internal/model/rotation.go
@@ -1,18 +1,24 @@
 package model
 
-// RotationCreateUpdateBase 创建/修改内容基类
+// RotationCreateUpdateBase 创建/修改轮播图基类
 type RotationCreateUpdateBase struct {
 	PicUrl string
 	Link   string
 	Sort   int
 }
 
-// RotationCreateInput 创建内容
+// RotationCreateInput 创建轮播图
 type RotationCreateInput struct {
 	RotationCreateUpdateBase
 }
 
-// RotationCreateOutput 创建内容返回结果
+// RotationCreateOutput 创建轮播图返回结果
 type RotationCreateOutput struct {
 	RotationId int `json:"rotation_id"`
+}
+
+// RotationUpdateInput 修改轮播图
+type RotationUpdateInput struct {
+	RotationCreateUpdateBase
+	Id int
 }
